fb5e852a547b16bc53180033a363b587b91c8419
Aliaksandr Kliamita <alexander.klemyato@gmail.com>
2025-10-20 14:50:59 +0300

    error handling

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db'); 
+const sequelize = require('./config/db'); 
+const models = require('./models/models');
+const cors = require('cors');
+const userRouter = require('./routes/userRouter');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', userRouter);
+
+//Error handling, last Middleware
+app.use(errorHandler);
+
+// app.get('/', (req, res) => { 
+//     res.status(200).json({ message: 'WORKING' });
+// })
 
 const start = async () => { 
     try {
