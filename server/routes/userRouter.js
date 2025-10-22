c537246a90d1895d1ee3933c903744996e3fac6d
Aliaksandr Kliamita <alexander.klemyato@gmail.com>
2025-10-22 09:58:17 +0300

    user authorization

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,15 @@
 const Router = require('express');
 const router = new Router();
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
 router.post('/registration', userController.registration);
 router.post('/login', userController.login);
-router.get('/auth', userController.auth);
+router.get('/auth', authMiddleware, userController.auth);
 
+
+router.get('/admin', authMiddleware, checkRoleMiddleware('ADMIN'), (req, res) => {
+    res.json({ message: 'Доступ только для админов' });
+});
 module.exports = router;
