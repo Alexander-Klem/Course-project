aed19bc0811c66ab2e9c71f4f9928a615c1aaa8c
Aliaksandr Kliamita <alexander.klemyato@gmail.com>
2025-11-06 14:54:26 +0300

    deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:7000/api',
-    headers: {
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:7000/api',
+  headers: {
     'Content-Type': 'application/json',
   }
 });
 
+// Интерцептор токена
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
