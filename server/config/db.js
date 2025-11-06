10929bed25696dd13fe25a7f582e183744c00c29
Aliaksandr Kliamita <alexander.klemyato@gmail.com>
2025-11-06 14:03:57 +0300

    deploy

--- server/config/db.js
+++ server/config/db.js
@@ -1,28 +1,13 @@
-// const { Sequelize } = require('sequelize');
 
-// module.exports = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//         dialect: 'postgres',
-//         host: process.env.DB_HOST,
-//         port: process.env.DB_PORT
-//     }
-// )
 
 const { Sequelize } = require('sequelize');
 
-// Внутренний URL PostgreSQL на Railway
-const urlDB = `postgresql://postgres:LMAHnETRSsDgRANKyKnSOoQAsdqIFRDo@postgres.railway.internal:5432/railway`;
-
-module.exports = new Sequelize(urlDB, {
+module.exports = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false  // Обязательно для Railway
+      rejectUnauthorized: false
     }
-  },
-  logging: false  // Убираем лишние логи
+  }
 });
