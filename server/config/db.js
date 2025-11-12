149878770152919765c94ade57ece85f1dc49a3f
Aliaksandr Kliamita <alexander.klemyato@gmail.com>
2025-11-12 18:53:23 +0300

    db

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
